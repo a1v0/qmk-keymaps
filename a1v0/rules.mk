dfc87a0e14a895c163bd023c43429b721d3a1c83
a1v0 <avc285@gmail.com>
2025-08-03 12:04:10 +0100

    Remove sendstring enablement, since it is not suitable for intended use

--- a1v0/rules.mk
+++ a1v0/rules.mk
@@ -11,7 +11,6 @@ RGBLIGHT_ENABLE 	= no	# Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE 	= no	# Enable one-hand typing
 OLED_ENABLE 		= yes	# OLED display
 COMBO_ENABLE        = yes   # Enable key combos (https://docs.qmk.fm/features/combo)
-SEND_STRING_ENABLE 	= yes   # Enable ability to send strings
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
