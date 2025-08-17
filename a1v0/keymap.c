4216e406749453a9ad9d5c741ee929087ce7171f
a1v0 <avc285@gmail.com>
2025-08-17 11:22:22 +0100

    Move alt and Windows keys closer to the centre

--- a1v0/keymap.c
+++ a1v0/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,                 KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, XXXXXXX, KC_BSPC, LT(_LOWER, KC_SPC),           LT(_RAISE, KC_ENT), KC_DEL, XXXXXXX, KC_RGUI
+                        XXXXXXX, KC_LALT, KC_BSPC, LT(_LOWER, KC_SPC),           LT(_RAISE, KC_ENT), KC_DEL, KC_RGUI, XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______, _______, _______,
 //   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
 //   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             KC_LWIN, _______, _______, _______,        _______,  _______, _______, KC_RWIN
+                            _______, KC_LWIN, _______, _______,        _______,  _______, KC_RWIN, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
