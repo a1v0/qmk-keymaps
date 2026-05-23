2ac0d1fda8e0459854d8a101f7edaff3fc5dae91
a1v0 <avc285@gmail.com>
2026-05-23 17:09:00 +0100

    Switch two rows of media keys, for ease of use

--- a1v0/keymap.c
+++ a1v0/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, _______, _______, KC_VOLD, KC_MUTE, KC_VOLU,                         _______, _______, _______,_______, _______, KC_F12,
-  _______, _______, _______, KC_BRID, _______, KC_BRIU,                         _______, _______, _______,_______, KC_GRAVE, _______,
+  _______, _______, _______, KC_BRID, _______, KC_BRIU,                         _______, _______, _______,_______, _______, KC_F12,
+  _______, _______, _______, KC_VOLD, KC_MUTE, KC_VOLU,                         _______, _______, _______,_______, KC_GRAVE, _______,
   _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_NONUS_HASH, _______, _______, _______,_______, KC_NUBS, _______,
 //   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
 //   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
